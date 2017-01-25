0a9e6b3b2521b57ed93d4e6f6b046d66a7c69eb7
Ace ARK <ace@Akhilas-MacBook-Pro.local>
2017-01-25 11:55:20 -0500

    Adding previously added code to the right handler.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,22 +27,9 @@ $("#signInWithGithub").on("click", function(){
 	  var token = result.credential.accessToken;
 	  // The signed-in user info.
 	  var user = result.user;
-	  // ...
-	}).catch(function(error) {
-		console.log("Entering error sign in.");
 
-	  // Handle Errors here.
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
-	  // The email of the user's account used.
-	  var email = error.email;
-	  // The firebase.auth.AuthCredential type that was used.
-	  var credential = error.credential;
-	  console.log("Error - " + errorCode + "  " + errorMessage + "  " + email + "  " + credential);
-
-	  $("#signOut").show();
-	  $("#signInWithGoogle, #signInWithGithub").hide();
-
+	  console.log("token - "+token);
+	  console.log("user - " + user);
 	  // Observer on Auth to see if user has really been signed in
 	  var user = firebase.auth().currentUser;
 
@@ -55,15 +42,31 @@ $("#signInWithGithub").on("click", function(){
 		// Getting provider-specific user info
 		var user = firebase.auth().currentUser;
 
-			if (user != null) {
-			  user.providerData.forEach(function (profile) {
-			    console.log("Sign-in provider: "+profile.providerId);
-			    console.log("  Provider-specific UID: "+profile.uid);
-			    console.log("  Name: "+profile.displayName);
-			    console.log("  Email: "+profile.email);
-			    console.log("  Photo URL: "+profile.photoURL);
-			  });
-			}
+		if (user != null) {
+		  user.providerData.forEach(function (profile) {
+		    console.log("Sign-in provider: "+profile.providerId);
+		    console.log("  Provider-specific UID: "+profile.uid);
+		    console.log("  Name: "+profile.displayName);
+		    console.log("  Email: "+profile.email);
+		    console.log("  Photo URL: "+profile.photoURL);
+		  });
+		}
+
+	  $("#signOut").show();
+	  $("#signInWithGoogle, #signInWithGithub").hide();
+
+	  // ...
+	}).catch(function(error) {
+		console.log("Entering error sign in.");
+
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  console.log("Error - " + errorCode + "  " + errorMessage + "  " + email + "  " + credential);
 
 	});
 });
